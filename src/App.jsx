306bad02e5023fa7e20a1e82ffcd747d59c12145
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-23 11:59:56 -0500

    Add additional styling; update classNames

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ScoreBoard from "./ScoreBoard/ScoreBoard";
 
 function App() {
     const key = "mnRGTWhmR1tglJdz7aq3Uc6sHGhbzlXy";
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=the+office&limit=8&offset=0&rating=pg&lang=en&bundle=messaging_non_clips`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=the+office&limit=9&offset=0&rating=pg&lang=en&bundle=messaging_non_clips`;
 
     const [gifs, setGifs] = useState([]);
 
@@ -21,18 +21,22 @@ function App() {
         };
     }, []);
 
+    console.log(gifs);
+
     return (
         <div className="main-container">
             <ScoreBoard />
-            {gifs.length > 0
-                ? gifs.map((gif, index) => {
-                      return (
-                          <div key={index}>
-                              <img src={gif.images.original.url} />
-                          </div>
-                      );
-                  })
-                : ""}
+            <div className="images-container">
+                {gifs.length > 0
+                    ? gifs.map((gif, index) => {
+                          return (
+                              <div key={index} className="game-images">
+                                  <img src={gif.images.fixed_height_downsampled.url} />
+                              </div>
+                          );
+                      })
+                    : ""}
+            </div>
         </div>
     );
 }
